51c7e041009511153a4297ec053c6e1018f67931
monirerahmani-dev <monirerahmani.dev@gmail.com>
2024-03-26 18:41:19 +0000

    docker groovy update

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -2,6 +2,7 @@
 package com.example
 
 class Docker implements Serializable {
+
     def script
 
     Docker (script) {
@@ -10,10 +11,15 @@ class Docker implements Serializable {
 
     def buildDockerImage(String imageName) {
         script.echo "building the docker image..."
-        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-            script.sh "docker build -t $imageName  ."
-            script.sh "echo '${script.$PASS}' | docker login -u '${script.USER}' --password-stdin"
-            script.sh "docker push $imageName"
+        script.sh "docker build -t $imageName  ."
+    }
+    def dockerLogin() {
+        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+            script.sh "echo '${script.PASS}' | docker login -u '${script.USER}' --password-stdin"
         }
+    }
+
+    def dockerPush(String imageName) {
+        script.sh "docker push $imageName"
     }
 }
